ad660fca1b2370b183764b7300fffd50d816560b
Martin AUBEUT <martin.aubeut@gmail.com>
2022-11-13 20:31:33 +0100

    add api without organisation

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 const express = require("express")
+var convert = require('xml-js');
 const app = express()
-const PORT = 3000
+const PORT = 4000
 
 
-const props = "Olivier Norek"
+const waitTillHTMLRendered = async (page, timeout = 30000) => {
+  const checkDurationMsecs = 1000;
+  const maxChecks = timeout / checkDurationMsecs;
+  let lastHTMLSize = 0;
+  let checkCounts = 1;
+  let countStableSizeIterations = 0;
+  const minStableSizeIterations = 3;
 
+  while(checkCounts++ <= maxChecks){
+    let html = await page.content();
+    let currentHTMLSize = html.length; 
 
-function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
+    let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+
+    console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+
+    if(lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize) 
+      countStableSizeIterations++;
+    else 
+      countStableSizeIterations = 0; //reset the counter
+
+    if(countStableSizeIterations >= minStableSizeIterations) {
+      console.log("Page rendered fully..");
+      break;
+    }
+
+    lastHTMLSize = currentHTMLSize;
+    await page.waitForTimeout(checkDurationMsecs);
+  }  
+};
+
+app.get('/api/:id', function(req, res, next) {
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
     });
- }
 
+    try {
+      const page = await browser.newPage();
+      await page.goto("http://catalogue.bnf.fr/api/test.do");
+      await page.type('input[name=val1]', req.params.id);
+      await page.click('input[value=Rechercher]');
+      await waitTillHTMLRendered(page)
+      const pages = await browser.pages()
+      const bodyHTML = await pages[2].evaluate(() => document.body.innerText);
+      res.send(bodyHTML);
+      console.log(bodyHTML);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await browser.close();
+    }
+  })();
+});
 
- 
- app.get('/api/{}', function(req, res) {
-   res.send('hello world');
- });
- 
  app.listen(PORT, function(err){
      if (err) console.log("Error in server setup")
      console.log("Server listening on Port", PORT);
@@ -27,32 +69,5 @@ function delay(time) {
 
 
 
-
-async function captureScreenshot() {
-  let browser = null;
-  try {
-    browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("http://catalogue.bnf.fr/api/test.do");
-    // Type into search box.
-    await page.type('input[name=val1]', props);
-    await page.click('input[value=Rechercher]');
-
-    // Wait for suggest overlay to appear and click "show all results".
-
-    console.log('before waiting');
-    await delay(4000);
-    console.log('after waiting');
-
-    await page.screenshot({path: 'bnfscreenshot.png'});
-  } catch (err) {
-    console.log(`❌ Error: ${err.message}`);
-  } finally {
-    await browser.close();
-    console.log(`\n🎉 GitHub profile screenshots captured.`);
-  }
-}
-
-captureScreenshot();
 
 
